75121ada85e7a61d2a8e29b47c2343f7ceec6097
thangnt-2930 <nguyen.tan.thang@sun-asterisk.com>
2025-09-18 11:10:02 +0700

    Init source base + Swagger + Versioning

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { setupSwagger } from './config/swagger.config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  // Versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: configService.get<string>('DEFAULT_API_VERSION') || '1',
+  });
+
+  // Swagger
+  setupSwagger(app);
+
+  // Start the server
+  await app.listen(configService.get<string>('PORT') || 3000);
+}
+bootstrap();
